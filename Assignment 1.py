307bde7948491c0580af54e3cbaa066503c76f2f
EstalinP <epenacanario9633@conestogac.on.ca>
2022-10-11 21:00:58 -0400

    Last version 2

--- Assignment 1.py
+++ Assignment 1.py
@@ -124,4 +124,4 @@ print("\t" "\t" "\t" "\t", "HST 13% ","\t" "\t",("${:.2f}").format(discount() * 
 print("\t" "\t" "\t" "\t" " " "Total",  " \t"  "\t", ("${:.2f}").format(discount() * 1.13), "CAD") #this prints the grand total of the recepit
 print("-" *75) #this creates the format of my recepit and is the last line 
 
-run = False #this will stpo the while loop
+run = False #this will stop the while loop
